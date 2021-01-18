edc440d8469d0c1253a85cc09c6c8959b50818e0
Nicho Rivera <30237172+rochinworks@users.noreply.github.com>
2021-01-18 13:27:11 -0700

    adds port as an env

--- src/main.go
+++ src/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"go-web-api/src/handler"
 	"go-web-api/src/pg"
 	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -12,6 +15,16 @@ func main() {
 	// ========================================
 	// connect to db and run migrations
 	// ========================================
+	if os.Getenv("ENV") != "production" && os.Getenv("DOCKER") != "true" {
+		wd, err := os.Getwd()
+		if err != nil {
+			log.Error("error getting working directory: ", err)
+		}
+		err = godotenv.Load(fmt.Sprint(wd, "/../.env"))
+		if err != nil {
+			log.Error("an error occurred loading the env file: ", err)
+		}
+	}
 	// connect
 	pgdb := pg.Connect()
 	// migrate
@@ -25,9 +38,10 @@ func main() {
 	h := handler.NewHandler(repo) // eventually connect the db here
 	r := httpRouter(h)
 
+	addr := fmt.Sprintf(":%s", os.Getenv("PORT"))
 	server := &http.Server{
 		Handler: r,
-		Addr:    ":8080", // env var
+		Addr:    addr, // env var
 	}
 	log.Info("server up and running at ", server.Addr)
 	log.Fatal(server.ListenAndServe())
--- src/pg/connect.go
+++ src/pg/connect.go
@@ -6,45 +6,29 @@ import (
 	"os"
 	"time"
 
-	"github.com/joho/godotenv"
 	log "github.com/sirupsen/logrus"
 )
 
 func Connect() *sql.DB {
 	pgChan := make(chan *sql.DB, 1)
-	wd, err := os.Getwd()
-	if err != nil {
-		log.Error("error getting working directory: ", err)
-	}
-
-	if os.Getenv("ENV") != "production" && os.Getenv("DOCKER") != "true" {
-		err = godotenv.Load(fmt.Sprint(wd, "/../.env"))
-		if err != nil {
-			log.Error("an error occurred loading the env file: ", err)
-		}
+	var pgDSN string
+	if os.Getenv("ENV") == "local" {
+		log.Info("I'm a local!")
+		pgHost := os.Getenv("POSTGRES_HOST")
+		pgUser := os.Getenv("POSTGRES_USER")
+		pgDBName := os.Getenv("POSTGRES_DB")
+		pgPass := os.Getenv("POSTGRES_PASSWORD")
+		// this string normally comes from the config (environment var)
+		pgDSN = fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable host=%s", pgUser, pgPass, pgDBName, pgHost)
+	} else {
+		pgDSN = os.Getenv("DATABASE_URL")
 	}
 
 	go func() {
-		var db *sql.DB
 
-		if os.Getenv("ENV") == "local" {
-			pgHost := os.Getenv("POSTGRES_HOST")
-			pgUser := os.Getenv("POSTGRES_USER")
-			pgDBName := os.Getenv("POSTGRES_DB")
-			pgPass := os.Getenv("POSTGRES_PASSWORD")
-			// this string normally comes from the config (environment var)
-			pgDSN := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable host=%s", pgUser, pgPass, pgDBName, pgHost)
-
-			// connect to the postgres DB
-			db, err = sql.Open("postgres", pgDSN)
-			if err != nil {
-				log.Fatal(fmt.Errorf("error connecting to postgres %+v", err))
-			}
-		} else {
-			db, err = sql.Open("postgres", os.Getenv("DATABASE_URL"))
-			if err != nil {
-				log.Fatal(fmt.Errorf("error connecting to postgres %+v", err))
-			}
+		db, err := sql.Open("postgres", pgDSN)
+		if err != nil {
+			log.Fatal(fmt.Errorf("error connecting to postgres %+v", err))
 		}
 
 		// Ping the db to make sure we connected properly
